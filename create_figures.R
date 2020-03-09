da545fd10aa8b84e5966d63b2a70fdbfee741f17
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-09 13:59:29 +0100

    Done

--- create_figures.R
+++ create_figures.R
@@ -1,39 +1,40 @@
+richel_repo_names <- c(
+  "richelbilderbeek/aureole",
+  "ropensci/babette",
+  paste0("richelbilderbeek/babette_example_", seq(1, 9)),
+  "richelbilderbeek/babetter",
+  "ropensci/beastier",
+  "ropensci/beautier",
+  "richelbilderbeek/becosys",
+  "richelbilderbeek/daisieme",
+  "richelbilderbeek/mcbette",
+  "ropensci/mauricer",
+  "richelbilderbeek/peregrine",
+  "richelbilderbeek/pirouette",
+  paste0("richelbilderbeek/pirouette_example_", seq(1, 30)),
+  "richelbilderbeek/raztr",
+  "richelbilderbeek/raztr",
+  "richelbilderbeek/raket",
+  "richelbilderbeek/razzo",
+  "richelbilderbeek/ribir",
+  "ropensci/tracerer"
+)
 # List the SLOC of all my projects
-richel_project_names <- c(
-  "aureole",
-  "babette",
-  paste0("babette_example_", seq(1, 9)),
-  "babetter",
-  "beastier",
-  "beautier",
-  "becosys",
-  "daisieme",
-  "mcbette",
-  "mauricer",
-  "peregrine",
-  "pirouette",
-  paste0("pirouette_example_", seq(1, 30)),
-  "raztr",
-  "raztr",
-  "raket",
-  "razzo",
-  "ribir",
-  "tracerer"
+richel_project_names <- basename(richel_repo_names)
+
+all_repo_names <- c(
+  richel_repo_names,
+  "CompEvol/beast2",
+  "rsetienne/DAISIE",
+  "rsetienne/DDD",
+  "Giappo/mbd",
+  "thijsjanzen/nLTT",
+  "thijsjanzen/nodeSub",
+  "rsetienne/PBD",
+  "klausVigo/phangorn"
 )
 
-
-project_names <- c(
-  richel_project_names,
-  "beast2",
-  "DAISIE",
-  "DDD",
-  "mbd",
-  "nLTT",
-  "nodeSub",
-  "PBD",
-  "phangorn"
-)
-
+project_names <- basename(all_repo_names)
 project_names <- sort(project_names)
 
 # Count non-whitespace lines
@@ -167,3 +168,95 @@ ggplot2::ggplot(
 #   handle = "GET /repos/:ropensci/:beautier/stargazers"
 # )
 
+# One
+count_n_stargazers <- function(
+  owner = "ropensci",
+  repo = "beautier"
+) {
+  length(gh::gh("GET /repos/:owner/:repo/stargazers", owner = owner, repo = repo, .limit = 1000))
+}
+
+# Many repos
+count_ns_stargazers <- function(
+  repos
+) {
+  n <- rep(x = 0, length(repos))
+  for (i in seq_along(repos)) {
+    n[i] <- count_n_stargazers(
+        dirname(repos[i]),
+        basename(repos[i])
+    )
+  }
+  n
+}
+
+df <- data.frame(repo = all_repo_names, n_stars = count_ns_stargazers(all_repo_names))
+df[ rev(order(df$n_stars)), ]
+#                                     repo n_stars
+# 57                       CompEvol/beast2     134
+# 64                    klausVigo/phangorn     110
+# 2                       ropensci/babette      20
+# 14                     ropensci/beautier       6
+# 56                     ropensci/tracerer       5
+# 13                     ropensci/beastier       5
+# 17              richelbilderbeek/mcbette       4
+# 61                      thijsjanzen/nLTT       3
+# 58                      rsetienne/DAISIE       3
+# 20            richelbilderbeek/pirouette       3
+# 54                richelbilderbeek/razzo       2
+# 19            richelbilderbeek/peregrine       2
+# 63                         rsetienne/PBD       1
+# 62                   thijsjanzen/nodeSub       1
+# 60                            Giappo/mbd       1
+# 59                         rsetienne/DDD       1
+# 18                     ropensci/mauricer       1
+# 16             richelbilderbeek/daisieme       1
+# 55                richelbilderbeek/ribir       0
+# 53                richelbilderbeek/raket       0
+# 52                richelbilderbeek/raztr       0
+# 51                richelbilderbeek/raztr       0
+# 50 richelbilderbeek/pirouette_example_30       0
+# 49 richelbilderbeek/pirouette_example_29       0
+# 48 richelbilderbeek/pirouette_example_28       0
+# 47 richelbilderbeek/pirouette_example_27       0
+# 46 richelbilderbeek/pirouette_example_26       0
+# 45 richelbilderbeek/pirouette_example_25       0
+# 44 richelbilderbeek/pirouette_example_24       0
+# 43 richelbilderbeek/pirouette_example_23       0
+# 42 richelbilderbeek/pirouette_example_22       0
+# 41 richelbilderbeek/pirouette_example_21       0
+# 40 richelbilderbeek/pirouette_example_20       0
+# 39 richelbilderbeek/pirouette_example_19       0
+# 38 richelbilderbeek/pirouette_example_18       0
+# 37 richelbilderbeek/pirouette_example_17       0
+# 36 richelbilderbeek/pirouette_example_16       0
+# 35 richelbilderbeek/pirouette_example_15       0
+# 34 richelbilderbeek/pirouette_example_14       0
+# 33 richelbilderbeek/pirouette_example_13       0
+# 32 richelbilderbeek/pirouette_example_12       0
+# 31 richelbilderbeek/pirouette_example_11       0
+# 30 richelbilderbeek/pirouette_example_10       0
+# 29  richelbilderbeek/pirouette_example_9       0
+# 28  richelbilderbeek/pirouette_example_8       0
+# 27  richelbilderbeek/pirouette_example_7       0
+# 26  richelbilderbeek/pirouette_example_6       0
+# 25  richelbilderbeek/pirouette_example_5       0
+# 24  richelbilderbeek/pirouette_example_4       0
+# 23  richelbilderbeek/pirouette_example_3       0
+# 22  richelbilderbeek/pirouette_example_2       0
+# 21  richelbilderbeek/pirouette_example_1       0
+# 15              richelbilderbeek/becosys       0
+# 12             richelbilderbeek/babetter       0
+# 11    richelbilderbeek/babette_example_9       0
+# 10    richelbilderbeek/babette_example_8       0
+# 9     richelbilderbeek/babette_example_7       0
+# 8     richelbilderbeek/babette_example_6       0
+# 7     richelbilderbeek/babette_example_5       0
+# 6     richelbilderbeek/babette_example_4       0
+# 5     richelbilderbeek/babette_example_3       0
+# 4     richelbilderbeek/babette_example_2       0
+# 3     richelbilderbeek/babette_example_1       0
+# 1               richelbilderbeek/aureole       0
+
+
+df
